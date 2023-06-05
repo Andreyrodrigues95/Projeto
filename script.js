fa325e15b9bf4acefdf4b328f386813009504bc0
Andreyrodrigues95 <121412905+Andreyrodrigues95@users.noreply.github.com>
2023-06-05 13:42:20 -0300

    JS-Readme updated

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  // Pegar a tag img
+  //    if(html.classList.contains('light')) {
+  //        html.classList.remove('light')
+  //    } else {
+  //        html.classList.add('light')}
+
   const img = document.querySelector("#profile img")
-  // Substituir a imagem
   if (html.classList.contains("light")) {
-    // Se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
-  } else {
-    // Se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar-dark.png")
   }
 }
